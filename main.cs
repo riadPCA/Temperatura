9473f1c901b01f9fb60c86403175468c9c508300
Riad Cure <100498353+riadPCA@users.noreply.github.com>
2022-03-08 01:04:15 +0000

    end commit

--- main.cs
+++ main.cs
@@ -3,7 +3,7 @@ using System;
 class Program {
   public static void Main (string[] args) {
 double Type, C, F,Resultado;
-    Console.WriteLine("¿Que escala Termica desea usuar?" );
+    Console.WriteLine("¿Que escala Termica desea convertir?" );
     Console.WriteLine("Para Celsius ingrese 1" );
     Console.WriteLine("Para fahrenheit ingrese 2");
     Type = Convert.ToDouble(Console.ReadLine());
@@ -12,14 +12,14 @@ double Type, C, F,Resultado;
         case 1:
             Console.WriteLine("Ingrese Los Celsius que desea convertir a fahrenheit");
             C = Convert.ToDouble(Console.ReadLine());
-            Resultado=C * 1,8 + 32;
+            Resultado=C * 1.8 + 32;
             Console.WriteLine("El equivalente de "+C+"ºC a fahrenheit es:           "+Resultado+"ºF");
             break;
 
         case 2:
             Console.WriteLine("Ingrese Los fahrenheit que desea convertir a Celsius");
             F = Convert.ToDouble(Console.ReadLine());
-            Resultado=(F-32) / 1,8;
+            Resultado=(F-32) / 1.8;
             Console.WriteLine("El equivalente de "+F+"ºF a Celsius es: "+Resultado+"ºC");
             break;
        default:
